6c49c310641234015c72551bbe12a02be20d3198
rushin236 <rushikeshshindesandesh@gmail.com>
2024-01-11 16:09:00 +0530

    yet to complete the streamlit application, just for saving the changes

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+import os
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+from streamlit_pages.recommend import main
+
+st.set_page_config(layout="wide")
+
+selected = option_menu(
+    menu_title="Phone Recommendation",
+    options=["Home", "Recommend", "About"],
+    icons=["house", "phone", "file-person"],
+    orientation="horizontal",
+    default_index=0,
+)
+
+if selected == "Home":
+    with open("./streamlit_pages/markdown/home_content.md", "r", encoding="UTF-8") as home_md_file:
+        home_content = home_md_file.read()
+
+    st.markdown(home_content)
+
+elif selected == "Recommend":
+    main()
+
+elif selected == "About":
+    with open("./streamlit_pages/markdown/about_content.md", "r", encoding="UTF-8") as about_md_file:
+        about_content = about_md_file.read()
+
+    st.markdown(about_content)
